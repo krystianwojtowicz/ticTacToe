6d0e07883c199eb1edc1b7838ac77f14af092f89
Krystian Wójtowicz <60481782+krystianwojtowicz@users.noreply.github.com>
2022-06-28 17:11:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+const gridBoxes = document.querySelectorAll('.grid-item');
+const p = document.querySelector('p');
+const h = document.querySelector('h1');
+
+p.textContent = 'kolej o';
+let xs = [];
+let os = [];
+const wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+
+
+
+let checker = (arr, target) => target.every(v => arr.includes(v));
+
+// const containsAlly = ys.every(element => {
+//     return wins[i].includes(element);
+// });
+
+
+Array.from(gridBoxes).forEach(element => {
+    element.addEventListener('click', (e) => {
+        if (p.textContent === 'kolej o') {
+            e.target.textContent = 'o';
+            p.textContent = 'kolej x';
+            os.push(parseInt(e.target.id));
+            for (let i =0; i<8; i++) {
+                if(checker(os, wins[i])) {
+                    h.textContent = 'zwycięża o';
+                } 
+            }
+
+       
+          
+        } else {
+            e.target.textContent = 'x';
+            p.textContent = 'kolej o';
+            xs.push(parseInt(e.target.id));
+            for (let i =0; i<8; i++) {
+                if(checker(xs, wins[i])) {
+                  h.textContent = 'zwycięża x';
+                }
+            }
+        }
+
+    })
+});
